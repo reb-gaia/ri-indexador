4e16bd89ae9377e9ef517d15c69a7a4bd1e59067
MatheusMeloGTD <matheus.melo@gotodata.com.br>
2023-05-10 08:17:23 -0300

    Atividade 4

--- index/structure.py
+++ index/structure.py
@@ -88,10 +88,15 @@ class TermOccurrence:
         return hash((self.doc_id, self.term_id))
 
     def __eq__(self, other_occurrence: "TermOccurrence"):
-        return False
+        return self.doc_id == other_occurrence.doc_id and self.term_id == other_occurrence.term_id
 
     def __lt__(self, other_occurrence: "TermOccurrence"):
-        return False
+        if self.term_id < other_occurrence.term_id:
+            return True
+        elif self.term_id == other_occurrence.term_id:
+            return self.doc_id < other_occurrence.doc_id
+        else:
+            return False
 
     def __str__(self):
         return f"( doc: {self.doc_id} term_id:{self.term_id} freq: {self.term_freq})"
