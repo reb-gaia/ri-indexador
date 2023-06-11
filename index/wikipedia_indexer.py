ad27153104a672703f0891145400c9fbe37ba8b5
Rebeca Gaia <reb.gaia@gmail.com>
2023-06-11 20:08:41 -0300

    Update wikipedia_indexer.py

--- index/wikipedia_indexer.py
+++ index/wikipedia_indexer.py
@@ -1,10 +1,14 @@
-from index.indexer import *
-from index.structure import *
+from indexer import *
+from structure import *
 
 if __name__ == "__main__":
-    HTMLIndexer.cleaner = Cleaner(stop_words_file="stopwords.txt",
-                        language="portuguese",
-                        perform_stop_words_removal=True,
-                        perform_accents_removal=True,
-                        perform_stemming=False)
-    
+    htmlIndexer = HTMLIndexer(FileIndex())
+    htmlIndexer.cleaner = Cleaner(stop_words_file="stopwords.txt",
+                                  language="portuguese",
+                                  perform_stop_words_removal=True,
+                                  perform_accents_removal=True,
+                                  perform_stemming=False)
+
+    htmlIndexer.index_text_dir("C:\\Users\\mathe\\Downloads\\ri-tp-wiki-data-master\\ri-tp-wiki-data-master")
+    htmlIndexer.index.write("wiki.idx")
+                               
